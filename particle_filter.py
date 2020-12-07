bfc5d73adabade19ece6d6383a3a0049dd9682a8
jdelaunay <julien.delaunay@insa-rouen.fr>
2020-12-07 21:21:09 +0100

    Update

--- particle_filter.py
+++ particle_filter.py
@@ -6,7 +6,8 @@ import numpy as np
 import argparse
 
 
-OBJ_DIM = (15, 30)
+OBJ_DIM = (15, 30, 3)
+CROSSHAIR_DIM = (15, 15)
 PARTICLE_SIGMA = np.min([OBJ_DIM]) // 4
 ESC = 27
 RED = (0, 0, 255)
@@ -14,14 +15,34 @@ GREEN = (0, 255, 0)
 
 
 def make_crosshairs(img, top_left, bottom_right, color_channel, captured):
-    obj_height, obj_width = OBJ_DIM
+    obj_height, obj_width = CROSSHAIR_DIM
+    center_x, center_y = img.shape[0] //2, img.shape[1] // 2
     img = cv2.rectangle(img, top_left, bottom_right, color_channel, 1)
-    #to complete
+    img = cv2.line(
+            img,
+            (center_x, img.shape[0] // 3),
+            (center_x, center_y - obj_height // 2),
+            color_channel, 1)
+    img = cv2.line(
+            img,
+            (center_x, center_y + obj_height // 2),
+            (center_x, img.shape[0] * 2 // 3),
+            color_channel, 1)
+    img = cv2.line(
+            img,
+            (img.shape[1] // 3, center_y),
+            (center_x - obj_width // 2, center_y),
+            color_channel, 1)
+    img = cv2.line(
+            img,
+            (center_x + obj_width // 2, center_y),
+            (img.shape[1] * 2 // 3, center_y),
+            color_channel, 1)
     return img
 
 def mark_target(img, center_xy, color_channel, captured):
     """Mark the target."""
-    obj_height, obj_width = OBJ_DIM
+    obj_height, obj_width, _ = OBJ_DIM
     center_x, center_y = int(center_xy[0]), int(center_xy[1])
 
     top_left_x = int(center_xy[0] - OBJ_DIM[1] // 2)
@@ -36,7 +57,22 @@ def mark_target(img, center_xy, color_channel, captured):
             1
         )
 
-    # to complete
+    img = cv2.line(
+            img,
+            (center_x, 0), (center_x, center_y - obj_height // 2),
+            color_channel, 1)
+    img = cv2.line(
+            img,
+            (center_x, center_y + obj_height // 2), (center_x, img.shape[0]),
+            color_channel, 1)
+    img = cv2.line(
+            img,
+            (0, center_y), (center_x - obj_width // 2, center_y),
+            color_channel, 1)
+    img = cv2.line(
+            img,
+            (center_x + obj_width // 2, center_y), (img.shape[1], center_y),
+            color_channel, 1)
     return img
 
 
@@ -44,7 +80,7 @@ def reset_particles_vars(particles_xy, particles_scores, particles_patches):
     return [], [], []
 
 
-def introduce_noise(particles_xy):
+def introduce_noise(particles_xy, img_height, img_width):
     """Introduce noise for the next generation."""
     for i, p in enumerate(particles_xy):
         if i == 0:
@@ -53,44 +89,41 @@ def introduce_noise(particles_xy):
         p[1] += np.random.normal(0, PARTICLE_SIGMA)
 
         # adjust for out_of_frame particles
-        p[0] = OBJ_DIM[1] if p[0] < OBJ_DIM[1] else p[0]
+        p[0] = OBJ_DIM[1] // 2 if p[0] < OBJ_DIM[1] else p[0]
         p[0] = img_width - OBJ_DIM[1] // 2 if p[0] > img_width - OBJ_DIM[1] // 2 else p[0]
-        p[1] = OBJ_DIM[1] if p[1] < OBJ_DIM[0] else p[1]
-        p[1] = img_width - OBJ_DIM[0] // 2 if p[1] > img_width - OBJ_DIM[0] // 2 else p[1]
+        p[1] = OBJ_DIM[0] // 2 if p[1] < OBJ_DIM[0] // 2 else p[1]
+        p[1] = img_height - OBJ_DIM[0] // 2 if p[1] > img_width - OBJ_DIM[0] // 2 else p[1]
     return particles_xy
 
 
-def display_to_check(particles_xy):
+def display_to_check(particles_xy, img_color):
     """Display the particles to check."""
     for p in particles_xy:
         img_color = cv2.circle(img_color,
-                (int(p[0]), int(p[1])),
-                1,
-                GREEN,
-                -1
-        )
-    pass
+                (int(p[0]), int(p[1])), 1, GREEN, -1)
+    return img_color
 
 
-def get_patches(particles_xy):
+def get_patches(particles_xy, img_color_clean):
     """Get patches for each particles."""
-    particles_patches = list()
+    particles_patches = []
     for p in particles_xy:
         patch_top_left_x = int(p[0] - OBJ_DIM[1] // 2)
         patch_top_left_y = int(p[1] - OBJ_DIM[0] // 2)
         patch_bottom_right_x = int(p[0] + OBJ_DIM[1] // 2)
         patch_bottom_right_y = int(p[1] + OBJ_DIM[0] // 2)
-        temp_patch = img.color_clean[patch_top_left_y : patch_bottom_right_y,
+        temp_patch = img_color_clean[patch_top_left_y : patch_bottom_right_y,
                                      patch_top_left_x : patch_bottom_right_x]
         particles_patches.append(temp_patch)
     return particles_patches
 
 
+
+
 def convert_to_proba(particles_scores):
     """Convert the scores to a probability."""
     particles_scores = np.array(particles_scores)
-    particles_scores = 1 / (2 * np.pi * ARGS.distribution_sigma) * np.exp(
-            -particles_scores / (2 * ARGS.distribution_sigma ** 2))
+    particles_scores = 1.0 / (2.0 * np.pi * ARGS.distribution_sigma) * np.exp(-particles_scores / (2.0 * ARGS.distribution_sigma ** 2))
     return particles_scores / np.sum(particles_scores)
 
 
@@ -98,9 +131,10 @@ def compare_patches(img_patch, particles_patches, particles_scores):
     """Compare each patch with the model patch."""
     model_patch = cv2.cvtColor(img_patch, cv2.COLOR_BGR2GRAY)
     model_patch = cv2.GaussianBlur(model_patch, (3, 3), 0)
+    particles_scores = [] # a retirer
     for p in particles_patches:
         temp_patch = cv2.cvtColor(p, cv2.COLOR_BGR2GRAY)
-        temp_patch = cv2.GaussianBlur(temp_patch, (5, 5), 0)
+        temp_patch = cv2.GaussianBlur(temp_patch, (3, 3), 0)
         mse = np.mean((model_patch - temp_patch) **2)
         particles_scores.append(mse)
     return convert_to_proba(particles_scores)
@@ -111,7 +145,7 @@ def resample(particles_xy, particles_scores):
     new_particles_xy_indexes = np.random.choice(
             range(ARGS.num_particles),
             size=ARGS.num_particles-1,
-            p=particles_scores,
+            p = particles_scores,
             replace=True
         )
     best_index = np.where(particles_scores == np.max(particles_scores))[0][0]
@@ -149,7 +183,7 @@ PARSER.add_argument(
 
 if __name__=="__main__":
     ARGS = PARSER.parse_args()
-    cam = cv2.VideoCapture(ARGS.video_path)
+    cam = cv2.VideoCapture(r'{}'.format(ARGS.video_path))
     captured = False
     img_patch = np.zeros(OBJ_DIM)
     particles_xy, particles_scores, particles_patches = [], [], []
@@ -157,18 +191,22 @@ if __name__=="__main__":
     if img is None:
         cam.release()
         sys.exit(0)
+
     img_color = cv2.resize(
         img,
         (int(img.shape[1]*ARGS.new_size_factor),
          int(img.shape[0]*ARGS.new_size_factor)))
     img_height, img_width, _ = img_color.shape
     top_left_x = img_width // 2 - OBJ_DIM[1] // 2
-    top_left_y = img_width // 2 - OBJ_DIM[0] // 2
+    top_left_y = img_height // 2 - OBJ_DIM[0] // 2
     bottom_right_x = img_width // 2 + OBJ_DIM[1] // 2
-    bottom_right_y = img_width // 2 + OBJ_DIM[0] // 2
+    bottom_right_y = img_height // 2 + OBJ_DIM[0] // 2
 
     while True:
-        ret, img = cam.read()
+        _, img = cam.read()
+        if img is None:
+            cam.release()
+            sys.exit(0)
         img_color = cv2.resize(
            img,
            (int(img.shape[1]*ARGS.new_size_factor),
@@ -177,51 +215,30 @@ if __name__=="__main__":
         img = cv2.cvtColor(img_color, cv2.COLOR_BGR2GRAY)
 
         if captured :
-            img_color = make_crosshairs(
-                    img_color,
-                    (top_left_x, top_left_y),
-                    (bottom_right_x, bottom_right_y),
-                    RED,
-                    1
-            )
-        else :
-            img_color = make_crosshairs(
-                    img_color,
-                    (top_left_x, top_left_y),
-                    (bottom_right_x, bottom_right_y),
-                    GREEN,
-                    1
-            )
-        cv2.imshow("Frame", img_color)
-
-        key = cv2.waitKey(10) & 0xFF
+            key = cv2.waitKey(10) & 0xFF
+        else:
+            key = cv2.waitKey(25) & 0xFF
         if key == ESC :
             break
 
-        if key== ord("c") :
-            particles_xy = list(particles_xy)
+        if (key == ord("c")) or (key == ord("C")):
             captured = True
             img_patch = img_color_clean[
                     top_left_y : bottom_right_y,
                     top_left_x : bottom_right_x]
-            for _ in range(ARGS.num_particles):
-                particles_xy.append([img_width // 2, img_height // 2])
-        elif key == ord("d") :
+            particles_xy = np.zeros((ARGS.num_particles, 2))
+            particles_xy[:, :] = [img_width // 2, img_height // 2]
+
+        elif (key == ord("d")) or (key == ord("D")) :
             captured = False
             img_patch = np.zeros(img_patch.shape)
-            reset_particles_vars(
-                    particles_xy,
-                    particles_scores,
-                    particles_patches
-                )
-
-        cv2.imshow("Patch", img_patch)
 
         if captured:
-            introduce_noise(particles_xy)
-            display_to_check(particles_xy)
-            particles_patches = get_patches(particles_xy)
-            particles_scores = compare_patches(img_patch, particles_patches)
+            particles_xy = introduce_noise(particles_xy, img_height, img_width)
+            img_color = display_to_check(particles_xy, img_color)
+            particles_patches = get_patches(particles_xy, img_color_clean)
+            particles_scores = compare_patches(img_patch, particles_patches,
+                    particles_scores)
             particles_xy, best_xy, best_index = resample(particles_xy,
                     particles_scores)
 
@@ -238,7 +255,6 @@ if __name__=="__main__":
                 1
             )
         cv2.imshow("Object Tracker", img_color)
-        cv2.waitKey(1)
 
     cam.release()
     cv2.destroyAllWindows()
